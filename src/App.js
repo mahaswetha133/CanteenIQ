5c396f24e1f9600efbb0c4af1e85e022c5c5256f
hariniprincya <your-github-email@example.com>
2025-09-22 16:47:12 +0000

    Fix deployment for GitHub Pages with HashRouter and homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Homes from "./Homes";
 import Login from "./Login";
 import Register from "./Register";
